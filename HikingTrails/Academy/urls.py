c69f9b23ad09f4caa499a3000cecf103a586ada3
Todor Mitrov <49582662+tod36@users.noreply.github.com>
2024-12-19 15:20:23 +0200

    with Academy meetings

--- HikingTrails/Academy/urls.py
+++ HikingTrails/Academy/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import MeetingCreateView, MeetingListView, MeetingPhotos, MeetingPhotosView
+
+from .views import MeetingCreateView, MeetingListView, MeetingPhotosView, MeetingPhotosDeleteView
 
 urlpatterns = [
     path('create/', MeetingCreateView.as_view(), name='meeting_form'),
 
     path('upload/', MeetingPhotosView.as_view(), name='upload_photo'),
+
+    path('delete/<int:pk>/', MeetingPhotosDeleteView.as_view(), name='delete_photo'),
+
     path('', MeetingListView.as_view(), name='photo_list'),
 ]
